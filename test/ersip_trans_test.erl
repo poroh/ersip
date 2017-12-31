3c9f4591d497d068f74c2c1f4d60254dc54ac8a2
Dmitry Poroh <dmitry@MacBook-Dmitry.local>
2017-12-31 16:35:43 +0300

    Transaction ID test is added

---
+++ test/ersip_trans_test.erl
@@ -0,0 +1,19 @@
+%%
+%% Copyright (c) 2017 Dmitry Poroh
+%% All rights reserved.
+%% Distributed under the terms of the MIT License. See the LICENSE file.
+%%
+%% Common SIP transaction interface tests
+%%
+
+-module(ersip_trans_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+transaction_id_test() ->
+    UASTid = <<"UAS tid">>,
+    { UAS, _ } = ersip_uas:new(UASTid, reliable, message, #{}),
+    ?assertEqual(UASTid, ersip_trans:id(UAS)),
+    UACTid = <<"UAC tid">>,
+    { UAC, _ } = ersip_uac:new(UACTid, reliable, message, #{}),
+    ?assertEqual(UACTid, ersip_trans:id(UAC)).

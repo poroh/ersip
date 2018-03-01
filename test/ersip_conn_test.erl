47d07387b540fe905908f251152ade72349e7474
Dmitry Poroh <dmitry@MacBook-Dmitry.local>
2018-03-02 01:02:15 +0300

    More connection tests added

--- test/ersip_conn_test.erl
+++ test/ersip_conn_test.erl
@@ -45,6 +45,7 @@ conn_add_received_test() ->
     MsgWithAnotherIPv4 =
         <<"INVITE sip:bob@biloxi.com SIP/2.0"
             ?crlf "Via: SIP/2.0/UDP 127.0.0.2;branch=z9hG4bK776asdhds"
+            ?crlf "Via: SIP/2.0/UDP 127.0.1.2;branch=z9hG4bK776ddedes"
             ?crlf "Max-Forwards: 70"
             ?crlf "To: Bob <sip:bob@biloxi.com>"
             ?crlf "From: Alice <sip:alice@atlanta.com>;tag=1928301774"
@@ -71,6 +72,27 @@ conn_add_received_test() ->
             ?crlf ?crlf "Test"
           >>,
     check_received(RemoteIP, MsgWithAnotherIPv6, Conn).
+
+conn_error_on_via_test() ->
+    RemoteIP = { 127, 0, 0, 1 },
+    Conn = create_conn(RemoteIP, 5090),
+    %% 1. received added if domain name:
+    MsgWithoutVia =
+        <<"INVITE sip:bob@biloxi.com SIP/2.0"
+          ?crlf "Max-Forwards: 70"
+          ?crlf "To: Bob <sip:bob@biloxi.com>"
+          ?crlf "From: Alice <sip:alice@atlanta.com>;tag=1928301774"
+          ?crlf "Call-ID: deadbeef",
+          ?crlf "CSeq: 314159 INVITE"
+          ?crlf "Contact: <sip:alice@pc33.atlanta.com>"
+          ?crlf "Content-Type: application/sdp"
+          ?crlf "Content-Length: 4"
+          ?crlf ?crlf "Test"
+        >>,
+    { _, [ { bad_message, BadMsg, { error, _ } } ] } = ersip_conn:conn_data(MsgWithoutVia, Conn),
+    CallIdH = ersip_msg:get(<<"call-id">>, BadMsg),
+    ?assertEqual(ersip_hdr_callid:make(<<"deadbeef">>), ersip_hdr_callid:make(CallIdH)).
+    
 
 conn_stream_test() ->
     RemoteIP = { 127, 0, 0, 1 },
@@ -105,6 +127,38 @@ conn_stream_test() ->
     CallId2H = ersip_msg:get(<<"call-id">>, NewMsg2),
     ?assertEqual(ersip_hdr_callid:make(<<"deadbeef">>), ersip_hdr_callid:make(CallId1H)),
     ?assertEqual(ersip_hdr_callid:make(<<"deadbeef1">>), ersip_hdr_callid:make(CallId2H)).
+
+conn_stream_no_content_len_test() ->
+    RemoteIP = { 127, 0, 0, 1 },
+    Conn = create_stream_conn(RemoteIP, 5090),
+    %% 1. received added if domain name:
+    Msgs =
+        <<"INVITE sip:bob@biloxi.com SIP/2.0"
+          ?crlf "Via: SIP/2.0/UDP pc33.atlanta.com;branch=z9hG4bK776asdhds"
+          ?crlf "Max-Forwards: 70"
+          ?crlf "To: Bob <sip:bob@biloxi.com>"
+          ?crlf "From: Alice <sip:alice@atlanta.com>;tag=1928301774"
+          ?crlf "Call-ID: deadbeef",
+          ?crlf "CSeq: 314159 INVITE"
+          ?crlf "Contact: <sip:alice@pc33.atlanta.com>"
+          ?crlf "Content-Type: application/sdp"
+          ?crlf "Content-Length: 4"
+          ?crlf ?crlf "Test"
+          "INVITE sip:bob@biloxi.com SIP/2.0"
+          ?crlf "Via: SIP/2.0/UDP pc33.atlanta.com;branch=z9hG4bK776asdhds"
+          ?crlf "Max-Forwards: 70"
+          ?crlf "To: Bob <sip:bob@biloxi.com>"
+          ?crlf "From: Alice <sip:alice@atlanta.com>;tag=1928301774"
+          ?crlf "Call-ID: deadbeef1",
+          ?crlf "CSeq: 314159 INVITE"
+          ?crlf "Contact: <sip:alice@pc33.atlanta.com>"
+          ?crlf "Content-Type: application/sdp"
+          ?crlf ?crlf "Test"
+        >>,
+    { _, [ { new_message, NewMsg1 }, { disconnect, { error, _ } } ] } = ersip_conn:conn_data(Msgs, Conn),
+    CallId1H = ersip_msg:get(<<"call-id">>, NewMsg1),
+    ?assertEqual(ersip_hdr_callid:make(<<"deadbeef">>), ersip_hdr_callid:make(CallId1H)).
+
 
 %%%===================================================================
 %%% Helpers

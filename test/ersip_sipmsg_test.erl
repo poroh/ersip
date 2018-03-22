63b16ec180082f58ab811803bb6575257c1e571a
Dmitry Poroh <poroh1981@yandex.ru>
2018-03-22 09:53:58 +0300

    Append all parsed headers to sipmsg is added

--- test/ersip_sipmsg_test.erl
+++ test/ersip_sipmsg_test.erl
@@ -43,6 +43,35 @@ parse_request_test() ->
     ?assertEqual({sent_by, { hostname, <<"pc33.atlanta.com">> }, 5060 },
                  ersip_hdr_via:sent_by(Via)).
 
+
+parse_request_append_all_test() ->
+    CallId = <<"a84b4c76e66710@pc33.atlanta.com">>,
+    Msg = <<"INVITE sip:bob@biloxi.com SIP/2.0"
+            ?crlf "Via: SIP/2.0/UDP pc33.atlanta.com;branch=z9hG4bK776asdhds"
+            ?crlf "Max-Forwards: 70"
+            ?crlf "To: Bob <sip:bob@biloxi.com>"
+            ?crlf "From: Alice <sip:alice@atlanta.com>;tag=1928301774"
+            ?crlf "Call-ID: ", CallId/binary,
+            ?crlf "CSeq: 314159 INVITE"
+            ?crlf "Contact: <sip:alice@pc33.atlanta.com>"
+            ?crlf "Content-Type: application/sdp"
+            ?crlf "Content-Length: 4"
+            ?crlf ?crlf "Test"
+          >>,
+    P  = ersip_parser:new_dgram(Msg),
+    { {ok, PMsg}, _P2 } = ersip_parser:parse(P),
+    { ok, SipMsg0 } = ersip_sipmsg:parse(PMsg,  [ from ]),
+    { ok, SipMsg } = ersip_sipmsg:parse(SipMsg0, all),
+    ?assertEqual(ersip_hdr_callid:make(CallId),        ersip_sipmsg:get(callid, SipMsg)),
+    ?assertEqual(ersip_hdr_maxforwards:make(<<"70">>), ersip_sipmsg:get(maxforwards, SipMsg)),
+    From = ersip_sipmsg:get(from, SipMsg),
+    To   = ersip_sipmsg:get(to, SipMsg),
+    ?assertEqual({tag, <<"1928301774">>},       ersip_hdr_fromto:tag(From)),
+    ?assertEqual({display_name, [ <<"Bob">> ]}, ersip_hdr_fromto:display_name(To)),
+    Via  = ersip_sipmsg:get(topmost_via, SipMsg),
+    ?assertEqual({sent_by, { hostname, <<"pc33.atlanta.com">> }, 5060 },
+                 ersip_hdr_via:sent_by(Via)).
+
 parse_response_test() ->
     CallId = <<"a84b4c76e66710@pc33.atlanta.com">>,
     Msg = <<"SIP/2.0 200 OK"

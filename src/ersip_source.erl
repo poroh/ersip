be77bc511c9ccbd70bf6cbc4dabbf56b4dbd52ce
Dmitry Poroh <poroh1981@yandex.ru>
2018-04-08 16:36:35 +0300

    Added source_id in source object

--- src/ersip_source.erl
+++ src/ersip_source.erl
@@ -10,6 +10,7 @@
 -module(ersip_source).
 
 -export([ new/3,
+          source_id/1,
           is_tls/1 ]).
 -export_type([ source/0 ]).
 
@@ -19,7 +20,7 @@
 
 -record(source, { peer      :: { ersip_host:host(), inet:port_number() },
                   transport :: ersip_transport:transport(),
-                  state     :: term()
+                  source_id :: term()
                 }).
 -type source() :: #source{}.
 
@@ -27,14 +28,18 @@
 %%% API
 %%%===================================================================
 
--spec new(Peer, ersip_transport:transport(), State) -> source() when
+-spec new(Peer, ersip_transport:transport(), SourceId) -> source() when
       Peer  :: { ersip_host:host(), inet:port_number() },
-      State :: term().
-new(Peer, Transport, State) ->
+      SourceId :: term().
+new(Peer, Transport, SourceId) ->
     #source{ peer = Peer,
              transport = Transport,
-             state = State
+             source_id = SourceId
            }.
+
+-spec source_id(source()) -> term().
+source_id(#source{ source_id = SId }) ->
+    SId.
 
 -spec is_tls(source()) -> boolean().
 is_tls(#source{ transport = T }) ->

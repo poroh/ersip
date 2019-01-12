e70228250a5cdcf3111285bac5d108bb9245c014
Dmitry Poroh <poroh1981@yandex.ru>
2019-01-12 12:10:56 +0300

    Added reason phrase for 503

--- src/message/ersip_status.erl
+++ src/message/ersip_status.erl
@@ -65,4 +65,5 @@ reason_impl(420) -> <<"Bad Extension">>;
 reason_impl(423) -> <<"Interval Too Brief">>;
 reason_impl(481) -> <<"Call/Transaction Does Not Exist">>;
 reason_impl(483) -> <<"Too many hops">>;
+reason_impl(503) -> <<"Service Unavailable">>;
 reason_impl(_)   -> <<"Unknown Status">>.

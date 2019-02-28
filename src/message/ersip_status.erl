faea3ff32514b97b30db0f57071d7f5ed3bf81f1
Dmitry Poroh <poroh1981@yandex.ru>
2019-02-28 14:22:34 +0300

    401/407 default reason phrase is added

--- src/message/ersip_status.erl
+++ src/message/ersip_status.erl
@@ -57,8 +57,10 @@ unsupported_uri_scheme_reason(URIScheme) ->
 %%%===================================================================
 reason_impl(100) -> <<"Trying">>;
 reason_impl(200) -> <<"OK">>;
+reason_impl(401) -> <<"Unauthorized">>;
 reason_impl(404) -> <<"Not Found">>;
 reason_impl(405) -> <<"Method Not Allowed">>;
+reason_impl(407) -> <<"Proxy Authentication Required">>;
 reason_impl(408) -> <<"Request Timeout">>;
 reason_impl(416) -> <<"Unsupported URI Scheme">>;
 reason_impl(420) -> <<"Bad Extension">>;

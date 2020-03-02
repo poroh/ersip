13b095a2dc1e9f7a2f4d823f4ee4eab2b4430abc
Dmitry Poroh <poroh1981@yandex.ru>
2020-03-02 19:39:04 +0300

    Default reason phrase for 500 is added

--- src/message/ersip_status.erl
+++ src/message/ersip_status.erl
@@ -70,5 +70,6 @@ reason_impl(423) -> <<"Interval Too Brief">>;
 reason_impl(481) -> <<"Call/Transaction Does Not Exist">>;
 reason_impl(483) -> <<"Too many hops">>;
 reason_impl(487) -> <<"Request Terminated">>;
+reason_impl(500) -> <<"Internal Server Error">>;
 reason_impl(503) -> <<"Service Unavailable">>;
 reason_impl(_)   -> <<"Unknown Status">>.

38e905b4a0ce76b6fabfb68b9927c1b5f8390345
0x42 <bpozdnyakov@gmail.com>
2022-05-02 14:15:28 +0300

    Rewrite fun parse, del macro, add test assemble

--- src/sdp/media/ersip_sdp_attr_rtpmap.erl
+++ src/sdp/media/ersip_sdp_attr_rtpmap.erl
@@ -3,7 +3,7 @@
 %% All rights reserved.
 %% Distributed under the terms of the MIT License. See the LICENSE file.
 %%
-%% SDP media attribute rtpmap 
+%% SDP media attribute rtpmap
 %%
 
 -module(ersip_sdp_attr_rtpmap).
@@ -13,7 +13,7 @@
 -export([payload_type/1]).
 -export([encoding_name/1]).
 -export([clock_rate/1]).
--export([encoding_params/1]). 
+-export([encoding_params/1]).
 -export([set_payload_type/2]).
 -export([set_encoding_name/2]).
 -export([set_clock_rate/2]).
@@ -23,11 +23,6 @@
 -export([assemble/1]).
 -export([assemble_bin/1]).
 
--define(IS_DIGIT(X),    (X >= $0 andalso X =< $9)).
--define(IS_CAP_CHAR(X), (X >= $A andalso X =< $Z)).
--define(IS_LOW_CHAR(X), (X >= $a andalso X =< $z)).
--define(IS_LETTER(X),   (?IS_CAP_CHAR(X) orelse ?IS_LOW_CHAR(X) orelse X == $- )).
-
 %%%===================================================================
 %%% Types
 %%%===================================================================
@@ -36,21 +31,23 @@
     type           :: payload_type(),
     encoding_name  :: encoding_name(),
     clock_rate     :: clock_rate(),
-    params         :: encoding_params() 
+    params         :: encoding_params()
 }).
 
--type payload_type()     :: binary().
+-type payload_type()     :: pos_integer().
 -type encoding_name()    :: binary().
 -type clock_rate()       :: pos_integer().
 -type encoding_params()  :: pos_integer() | undefined.
 -type rtpmap()           :: #rtpmap{}.
--type parse_result()     :: parse_result(rtpmap()).
+-type parse_result()     :: {ok, rtpmap()} |
+                            {error, {invalid_rtpmap_candidate, term()}}.
 -type parse_result(T)    :: ersip_parser_aux:parse_result(T).
 
 -export_type([
     payload_type/0,
     clock_rate/0,
-    encoding_name/0    
+    encoding_name/0,
+    encoding_params/0
 ]).
 
 %%%===================================================================
@@ -117,7 +114,7 @@ assemble_bin(Rtpmap) ->
 %%%===================================================================
 %%% Internal implementation
 %%%===================================================================
-%% https://datatracker.ietf.org/doc/html/rfc4566#section-6
+%% https://datatracker.ietf.org/doc/html/rfc8866#section-6.6
 %%
 %% a=rtpmap:<payload type> <encoding name>/<clock rate> [/<encoding parameters>]
 %%
@@ -125,85 +122,53 @@ assemble_bin(Rtpmap) ->
 -spec do_parse_rtpmap(binary()) -> parse_result().
 do_parse_rtpmap(Bin) ->
     Parsers = [
-        fun parse_payload_type/1,
-        fun parse_encoding_name/1,
-        fun parse_clock_rate/1,
+        fun ersip_parser_aux:parse_non_neg_int/1,
+        fun ersip_parser_aux:trim_lws/1,
+        fun ersip_sdp_aux:parse_token/1,
+        fun(B) -> ersip_parser_aux:parse_sep($/, B) end,
+        fun ersip_parser_aux:parse_non_neg_int/1,
         fun parse_encoding_params/1
     ],
-    case ersip_parser_aux:parse_all(Bin, Parsers) of 
-        {ok, Result, Rest} ->
-            [Type, Name, ClockRate, EncodingParams] = Result,
+    case ersip_parser_aux:parse_all(Bin, Parsers) of
+        {ok, Result, _} ->
+            [Type, _Lws, Name, _Sep, ClockRate, EncodingParams] = Result,
             {ok, #rtpmap{
                 type = Type,
                 encoding_name = Name,
                 clock_rate = ClockRate,
                 params = EncodingParams
-            }, Rest};
+            }};
         {error, Reason} ->
             {error, {invalid_rtpmap_candidate, Reason}}
     end.
-
--spec parse_payload_type(binary()) -> parse_result(payload_type()).
-parse_payload_type(Bin) ->
-    parse_payload_type(Bin, <<>>).
-
-parse_payload_type(<<H:8, Tail/binary>>, Acc) when ?IS_DIGIT(H) ->
-    parse_payload_type(Tail, <<Acc/binary, H:8>>);
-
-parse_payload_type(<<" ", _Tail/binary>>, <<>>) ->
-    {error, invalid_payload_type};
-parse_payload_type(<<" ", Tail/binary>>, PayloadType) ->
-    {ok, PayloadType, Tail};
-parse_payload_type(_, _) ->
-    {error, invalid_payload_type}.
-
--spec parse_encoding_name(binary()) -> parse_result(encoding_name()).
-parse_encoding_name(Bin) ->
-    parse_encoding_name(Bin, <<>>).
-
-parse_encoding_name(<<"/", Tail/binary>>, EncodingName) ->
-    {ok, EncodingName, Tail};
-parse_encoding_name(<<H:8, Tail/binary>>, Acc) when ?IS_LETTER(H) orelse ?IS_DIGIT(H) ->
-    parse_encoding_name(Tail, <<Acc/binary, H:8>>);
-parse_encoding_name(_, _) ->
-    {error, invalid_encoding_name}.
-
--spec parse_clock_rate(binary()) -> parse_result(clock_rate()).
-parse_clock_rate(Bin) ->
-    parse_clock_rate(Bin, <<>>).
-
-parse_clock_rate(<<>>, ClockRate) ->
-    {ok, binary_to_integer(ClockRate), <<>>};
-parse_clock_rate(<<"/", EncodingParams/binary>>, ClockRate) ->
-    {ok, binary_to_integer(ClockRate), EncodingParams};
-parse_clock_rate(<<H:8, Tail/binary>>, Acc) when ?IS_DIGIT(H) ->
-    parse_clock_rate(Tail, <<Acc/binary, H:8>>);
-parse_clock_rate(_, _) ->
-    {error, invalid_clock_rate}.
 
 -spec parse_encoding_params(binary()) -> parse_result(encoding_params()).
 parse_encoding_params(<<>>) ->
     {ok, undefined, <<>>};
 parse_encoding_params(Bin) ->
-    try 
-        {ok, binary_to_integer(Bin), <<>>}
-    catch error:badarg ->
-        {error, invalid_encoding_params}        
+    Parsers = [
+        fun(B) -> ersip_parser_aux:parse_sep($/, B) end,
+        fun ersip_parser_aux:parse_non_neg_int/1
+    ],
+    case ersip_parser_aux:parse_all(Bin, Parsers) of
+        {ok, [_, EncodingParams], Rest} ->
+            {ok, EncodingParams, Rest};
+        Error ->
+            Error
     end.
 
 -spec assemble_rtpmap(rtpmap()) -> iolist().
 assemble_rtpmap(#rtpmap{} = Rtpmap) ->
     ClockRateBin = integer_to_binary(clock_rate(Rtpmap)),
     [
-        payload_type(Rtpmap), " ",
+        integer_to_binary(payload_type(Rtpmap)), " ",
         encoding_name(Rtpmap), "/",
         ClockRateBin,
-        encoding_params_assemble(Rtpmap)
+        encoding_params_assemble(encoding_params(Rtpmap))
     ].
 
--spec encoding_params_assemble(rtpmap() | undefined) -> iolist().
+-spec encoding_params_assemble(encoding_params()) -> iolist().
 encoding_params_assemble(undefined) ->
     [];
-encoding_params_assemble(Rtpmap) ->
-    EncodingParams = encoding_params(Rtpmap),
+encoding_params_assemble(EncodingParams) ->
     ["/", integer_to_binary(EncodingParams)].
--- test/sdp/media/ersip_sdp_attr_rtpmap_test.erl
+++ test/sdp/media/ersip_sdp_attr_rtpmap_test.erl
@@ -3,7 +3,7 @@
 %% All rights reserved.
 %% Distributed under the terms of the MIT License. See the LICENSE file.
 %%
-%% SDP media attribute rtpmap 
+%% SDP media attribute rtpmap
 %%
 
 -module(ersip_sdp_attr_rtpmap_test).
@@ -15,29 +15,43 @@
 %%%===================================================================
 
 parse_test() ->
-    {ok, Rtpmap0, _} = ersip_sdp_attr_rtpmap:parse(<<"99 AMR-WB/16000">>),
-    ?assertEqual(<<"99">>,          ersip_sdp_attr_rtpmap:payload_type(Rtpmap0)),
+    RtpmapBin0 = <<"99 AMR-WB/16000">>,
+    {ok, Rtpmap0} = ersip_sdp_attr_rtpmap:parse(RtpmapBin0),
+    ?assertEqual(99,                ersip_sdp_attr_rtpmap:payload_type(Rtpmap0)),
     ?assertEqual(<<"AMR-WB">>,      ersip_sdp_attr_rtpmap:encoding_name(Rtpmap0)),
     ?assertEqual(16000,             ersip_sdp_attr_rtpmap:clock_rate(Rtpmap0)),
     ?assertEqual(undefined,         ersip_sdp_attr_rtpmap:encoding_params(Rtpmap0)),
+    ?assertEqual(RtpmapBin0,        ersip_sdp_attr_rtpmap:assemble_bin(Rtpmap0)),
 
-    {ok, Rtpmap1, _} = ersip_sdp_attr_rtpmap:parse(<<"100 H264/90000/2">>),
-    ?assertEqual(<<"100">>,         ersip_sdp_attr_rtpmap:payload_type(Rtpmap1)),
+    RtpmapBin1 = <<"100 H264/90000/2">>,
+    {ok, Rtpmap1} = ersip_sdp_attr_rtpmap:parse(RtpmapBin1),
+    ?assertEqual(100,               ersip_sdp_attr_rtpmap:payload_type(Rtpmap1)),
     ?assertEqual(<<"H264">>,        ersip_sdp_attr_rtpmap:encoding_name(Rtpmap1)),
     ?assertEqual(90000,             ersip_sdp_attr_rtpmap:clock_rate(Rtpmap1)),
     ?assertEqual(2,                 ersip_sdp_attr_rtpmap:encoding_params(Rtpmap1)),
+    ?assertEqual(RtpmapBin1,        ersip_sdp_attr_rtpmap:assemble_bin(Rtpmap1)),
 
-    {ok, Rtpmap2, _} = ersip_sdp_attr_rtpmap:parse(<<"101 telephone-event/8000">>),
-    ?assertEqual(<<"101">>,             ersip_sdp_attr_rtpmap:payload_type(Rtpmap2)),
+    {ok, Rtpmap2} = ersip_sdp_attr_rtpmap:parse(<<"101 telephone-event/8000">>),
+    ?assertEqual(101,                   ersip_sdp_attr_rtpmap:payload_type(Rtpmap2)),
     ?assertEqual(<<"telephone-event">>, ersip_sdp_attr_rtpmap:encoding_name(Rtpmap2)),
     ?assertEqual(8000,                  ersip_sdp_attr_rtpmap:clock_rate(Rtpmap2)),
     ?assertEqual(undefined,             ersip_sdp_attr_rtpmap:encoding_params(Rtpmap2)),
+
     ok.
 
 parse_error_test() ->
-    ?assertEqual({error, {invalid_rtpmap_candidate, invalid_clock_rate}},  ersip_sdp_attr_rtpmap:parse(<<"99 AMR-WB/16000A">>)),
-    ?assertEqual({error, {invalid_rtpmap_candidate, invalid_payload_type}},  ersip_sdp_attr_rtpmap:parse(<<" AMR-WB/16000">>)),
-    ?assertEqual({error, {invalid_rtpmap_candidate, invalid_encoding_params}},  ersip_sdp_attr_rtpmap:parse(<<"0 AMR-WB/16000/A">>)),
+    ?assertEqual(
+        {error, {invalid_rtpmap_candidate, {no_separator, $/, <<"A">>}}},
+        ersip_sdp_attr_rtpmap:parse(<<"99 AMR-WB/16000A">>)
+    ),
+    ?assertEqual(
+        {error, {invalid_rtpmap_candidate, {invalid_integer, <<" AMR-WB/16000">>}}},
+        ersip_sdp_attr_rtpmap:parse(<<" AMR-WB/16000">>)
+    ),
+    ?assertEqual(
+        {error, {invalid_rtpmap_candidate, {invalid_integer, <<"A">>}}},
+        ersip_sdp_attr_rtpmap:parse(<<"0 AMR-WB/16000/A">>)
+    ),
     ok.
 
 
